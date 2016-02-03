760b098c306147ad0a0a763b2896d86be0769956
Yuriy Ankudinov <stremlenye@gmail.com>
2016-02-03 11:21:46 +0100

    Basic setup

---
+++ setup.sh
@@ -0,0 +1,36 @@
+brew install ant
+brew install bash-completion
+brew install cask
+brew install docker
+brew install docker-machine
+brew install emacs
+brew install gdbm
+brew install git
+brew install go
+brew install gradle
+brew install groovy
+brew install httpie
+brew install maven
+brew install mongodb
+brew install node
+brew install openssl
+brew install postgresql
+brew install python
+brew install readline
+brew install redis
+brew install sbt
+brew install scala
+brew install sqlite
+brew install thefuck
+brew install watch
+brew cask install atom
+brew cask install intellij-idea
+brew cask install java
+brew cask install limechat
+brew cask install qlmarkdown
+brew cask install skype
+brew cask install slack
+brew cask install spotify
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
